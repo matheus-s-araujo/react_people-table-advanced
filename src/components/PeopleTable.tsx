b504b8cc8bcd5a2eaa68c80dcb5704c98b44097a
Matheus <theus.sousaa@gmail.com>
2026-03-05 10:50:44 -0300

    finished project

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -3,14 +3,78 @@
 import classNames from 'classnames';
 import { Person } from '../types';
 import { PersonLink } from './PersonLink';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
 
 type PeopleTableProps = {
-  people: Person[] | null;
+  people: Person[];
 };
 
 export const PeopleTable = ({ people }: PeopleTableProps) => {
   const { slug } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [peopleSortered, setPeopleSortered] = useState<Person[]>(people);
+
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
+
+  const sorteredPeople = (newFilter: string) => {
+    if (order === 'desc' && sort === newFilter) {
+      searchParams.delete('sort');
+      searchParams.delete('order');
+
+      setPeopleSortered(people);
+      setSearchParams(searchParams);
+
+      return;
+    }
+
+    let newPeopleSortered = [...peopleSortered];
+
+    switch (newFilter) {
+      case 'name':
+        newPeopleSortered = newPeopleSortered.sort((a, b) =>
+          a.name.localeCompare(b.name),
+        );
+
+        break;
+      case 'sex':
+        newPeopleSortered = newPeopleSortered.sort((a, b) =>
+          a.sex.localeCompare(b.sex),
+        );
+
+        break;
+      case 'born':
+        newPeopleSortered = newPeopleSortered.sort((a, b) => a.born - b.born);
+        break;
+      case 'died':
+        newPeopleSortered = newPeopleSortered.sort((a, b) => a.died - b.died);
+        break;
+    }
+
+    if (sort === newFilter) {
+      searchParams.set('order', 'desc');
+      newPeopleSortered = newPeopleSortered.reverse();
+    }
+
+    searchParams.set('sort', newFilter);
+
+    setPeopleSortered(newPeopleSortered);
+    setSearchParams(searchParams);
+  };
+
+  const handleSortIcon = (column: string): string => {
+    // console.log({ column, sort, order });
+    if (column === sort) {
+      if (order === 'desc') {
+        return 'fa-sort-down';
+      }
+
+      return 'fa-sort-up';
+    }
+
+    return 'fa-sort';
+  };
 
   const hasParentOnTheList = (parentName: string) => {
     return people?.find(person => person.name === parentName);
@@ -27,17 +91,73 @@ export const PeopleTable = ({ people }: PeopleTableProps) => {
     >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Name
+              <a onClick={() => sorteredPeople('name')}>
+                <span className="icon">
+                  <i
+                    className={classNames('fas', {
+                      [handleSortIcon('name')]: true,
+                    })}
+                  />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Sex
+              <a onClick={() => sorteredPeople('sex')}>
+                <span className="icon">
+                  <i
+                    className={classNames('fas', {
+                      [handleSortIcon('sex')]: true,
+                    })}
+                  />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Born
+              <a onClick={() => sorteredPeople('born')}>
+                <span className="icon">
+                  <i
+                    className={classNames('fas', {
+                      [handleSortIcon('born')]: true,
+                    })}
+                  />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Died
+              <a onClick={() => sorteredPeople('died')}>
+                <span className="icon">
+                  <i
+                    className={classNames('fas', {
+                      [handleSortIcon('died')]: true,
+                    })}
+                  />
+                </span>
+              </a>
+            </span>
+          </th>
+
           <th>Mother</th>
           <th>Father</th>
         </tr>
       </thead>
 
       <tbody>
-        {people?.map(person => (
+        {peopleSortered.map(person => (
           <tr
             data-cy="person"
             key={person.name}
